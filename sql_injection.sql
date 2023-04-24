e31ce2f60931f36f5b68a0da537d7ac6b79e74a9
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-04-25 01:01:17 +0530

    Update sql_injection.sql

--- sql_injection.sql
+++ sql_injection.sql
@@ -1,1 +1,7 @@
 SELECT * FROM users WHERE name='<name>' and password='' OR '1'='1' 
+
+CREATE TABLE PassHack (message varchar(255));
+
+INSERT INTO PassHack (message) VALUES ('Cracked');
+
+SELECT * FROM PassHack;
