b0fb4b499d4daceea1776bd310b06b8d20bcb6ac
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-05-19 18:21:19 +0530

    Update sql_injection.sql

--- sql_injection.sql
+++ sql_injection.sql
@@ -5,3 +5,12 @@ CREATE TABLE PassHack (message varchar(255));
 INSERT INTO PassHack (message) VALUES ('Cracked');
 
 SELECT * FROM PassHack;
+
+
+SELECT * FROM accounts WHERE username='Simran' AND password='''
+
+SELECT * FROM accounts WHERE username='Simran' AND password='123' and 1=1#''
+
+SELECT * FROM accounts WHERE username='Simran' AND password='aaa' and 1=1#''
+
+SELECT * FROM accounts WHERE username='admin#' AND password='1'
